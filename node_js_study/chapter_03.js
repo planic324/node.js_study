a532e129ae59231b069fcd29bbeab909afbfc34f
RohEunChan <nong0324@naver.com>
2022-12-07 16:41:07 +0900

    [공부] node.js 교과서  - 3장 모듈 만들기 진행 중

--- node_js_study/chapter_03.js
+++ node_js_study/chapter_03.js
@@ -1,11 +1,33 @@
-function helloWorld(){
-    console.log('hellow world');
-    helloNode();
+// 3.2 JS 파일 실행기
+// function helloWorld(){
+//     console.log('hellow world');
+//     helloNode();
+// }
+
+// function helloNode(){
+//     console.log('hellow node');
+// }
+
+// helloWorld();
+
+// 3.3 모듈로 만들기
+// 모듈 제작
+const odd = '홀수입니다';
+const even = '짝수입니다';
+
+module.exports = {
+    odd,
+    even,
+};
+
+// 참조하는 방법
+const {odd, even} = require('./파일명');
+
+function checkOddOrEven(num){
+    if(num % 2){
+        return odd;
+    }
+    return even;
 }
 
-function helloNode(){
-    console.log('hellow node');
-    helloNode();
-}
-
-helloWorld();
+module.exports = checkOddOrEven
